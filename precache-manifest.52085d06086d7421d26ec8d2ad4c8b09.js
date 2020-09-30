5ee98e4e0e1a92a19887466c87d4fabfa68da594
AlexSavchuk95 <savchukalex57@gmail.com>
2020-09-30 17:12:21 +0300

    Updates

---
+++ precache-manifest.52085d06086d7421d26ec8d2ad4c8b09.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf8df6f14eeea46c4b3abdc83e5683e",
+    "url": "/LeuloProjectv2.0/index.html"
+  },
+  {
+    "revision": "0e723df4d4e8880ad00d",
+    "url": "/LeuloProjectv2.0/static/css/2.e9a63919.chunk.css"
+  },
+  {
+    "revision": "d2671805ff963087c7b2",
+    "url": "/LeuloProjectv2.0/static/css/main.7441f8e5.chunk.css"
+  },
+  {
+    "revision": "0e723df4d4e8880ad00d",
+    "url": "/LeuloProjectv2.0/static/js/2.a4f237d6.chunk.js"
+  },
+  {
+    "revision": "ec6a03abad0b8bc7870afc7f1901a4c5",
+    "url": "/LeuloProjectv2.0/static/js/2.a4f237d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2671805ff963087c7b2",
+    "url": "/LeuloProjectv2.0/static/js/main.3ecafbce.chunk.js"
+  },
+  {
+    "revision": "ab54e050d72125fd83f3",
+    "url": "/LeuloProjectv2.0/static/js/runtime-main.994d4577.js"
+  },
+  {
+    "revision": "1cffd5f72d52a7997abd6df918f73cb9",
+    "url": "/LeuloProjectv2.0/static/media/1.1cffd5f7.png"
+  },
+  {
+    "revision": "7c74d6b805e0df38712faf658369df23",
+    "url": "/LeuloProjectv2.0/static/media/10.7c74d6b8.png"
+  },
+  {
+    "revision": "0411b32c3971828bb598deb195119687",
+    "url": "/LeuloProjectv2.0/static/media/11.0411b32c.png"
+  },
+  {
+    "revision": "e006427cc616b85fea37bfd0be3b1225",
+    "url": "/LeuloProjectv2.0/static/media/12.e006427c.png"
+  },
+  {
+    "revision": "188ccf794eead763c44e73dfbd4e1699",
+    "url": "/LeuloProjectv2.0/static/media/13.188ccf79.png"
+  },
+  {
+    "revision": "f80b866c8938eb9a689a81d4a3426eb1",
+    "url": "/LeuloProjectv2.0/static/media/14.f80b866c.png"
+  },
+  {
+    "revision": "71d0411248e9be44d86e28b2a562345f",
+    "url": "/LeuloProjectv2.0/static/media/15.71d04112.png"
+  },
+  {
+    "revision": "735b91fe7086faf179524c380896aa77",
+    "url": "/LeuloProjectv2.0/static/media/16.735b91fe.png"
+  },
+  {
+    "revision": "5e45732250025ebfd268e8e79398a6c8",
+    "url": "/LeuloProjectv2.0/static/media/17.5e457322.png"
+  },
+  {
+    "revision": "abf758ee32857f61d5595f0d387aaedf",
+    "url": "/LeuloProjectv2.0/static/media/18.abf758ee.png"
+  },
+  {
+    "revision": "01c05218356184830eed99ce6395e296",
+    "url": "/LeuloProjectv2.0/static/media/19.01c05218.png"
+  },
+  {
+    "revision": "6405b0a0b5acc2bd166cdf607f995658",
+    "url": "/LeuloProjectv2.0/static/media/2.6405b0a0.png"
+  },
+  {
+    "revision": "38db041a2934ccf8e00fcef29f5d203f",
+    "url": "/LeuloProjectv2.0/static/media/20.38db041a.png"
+  },
+  {
+    "revision": "58a3783525c65446f32473b1692ea4d5",
+    "url": "/LeuloProjectv2.0/static/media/3.58a37835.png"
+  },
+  {
+    "revision": "77cbcfa09f29d17a2f6d1173a9bc86ad",
+    "url": "/LeuloProjectv2.0/static/media/4.77cbcfa0.png"
+  },
+  {
+    "revision": "704349b44e7180771748cc246e3fc16b",
+    "url": "/LeuloProjectv2.0/static/media/5.704349b4.png"
+  },
+  {
+    "revision": "b18adfe4290ea26c89cde64d76a0f8a2",
+    "url": "/LeuloProjectv2.0/static/media/6.b18adfe4.png"
+  },
+  {
+    "revision": "a1319e054c4e91bbc103cdf67baeaf9a",
+    "url": "/LeuloProjectv2.0/static/media/7.a1319e05.png"
+  },
+  {
+    "revision": "cd29fc41d4d4970bc0fc861b566fd095",
+    "url": "/LeuloProjectv2.0/static/media/8.cd29fc41.png"
+  },
+  {
+    "revision": "33d008455e5527d98f5327c7097fde5a",
+    "url": "/LeuloProjectv2.0/static/media/9.33d00845.png"
+  },
+  {
+    "revision": "cdb30e3adf6b98d5d31ac28c0e2264ae",
+    "url": "/LeuloProjectv2.0/static/media/B12.cdb30e3a.png"
+  },
+  {
+    "revision": "7a74048f1e27128a22ceb328ed1b0026",
+    "url": "/LeuloProjectv2.0/static/media/Bouncebackblend.7a74048f.png"
+  },
+  {
+    "revision": "71ec02a466a1f1b0ac38973c82d18aa7",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Black.71ec02a4.eot"
+  },
+  {
+    "revision": "a2ac97d6ee9f4d18398c6d5ff575fcc3",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Black.a2ac97d6.woff"
+  },
+  {
+    "revision": "c34918c9f6faeba145e256b4b073061b",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Black.c34918c9.woff2"
+  },
+  {
+    "revision": "4ad4f4912586937f89c42bba94fdaa9d",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BlackItalic.4ad4f491.woff"
+  },
+  {
+    "revision": "77bb7e8bcc70501403e3cc9820e12714",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BlackItalic.77bb7e8b.eot"
+  },
+  {
+    "revision": "9f3e1f759030704698b7bad19c9e4fa4",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BlackItalic.9f3e1f75.woff2"
+  },
+  {
+    "revision": "18733b6850d5c35c1e86688d1185f2a0",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Bold.18733b68.woff"
+  },
+  {
+    "revision": "5fb55beeba2d83776ed2418aa0a14e24",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Bold.5fb55bee.woff2"
+  },
+  {
+    "revision": "a4dcc8ed733b9d673667f1d04476195a",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Bold.a4dcc8ed.eot"
+  },
+  {
+    "revision": "04271c88e189eeea660de505a9b3b8a3",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BoldItalic.04271c88.eot"
+  },
+  {
+    "revision": "38d03618e4493dd8999b12b7b5453299",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BoldItalic.38d03618.woff"
+  },
+  {
+    "revision": "d6d45c07e094f4f06977e868ab5474d4",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-BoldItalic.d6d45c07.woff2"
+  },
+  {
+    "revision": "5980b1ffe159569d242ad5668d8ec9cb",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Heavy.5980b1ff.woff2"
+  },
+  {
+    "revision": "6e06205e398bfea008ecf9b2a68b5c33",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Heavy.6e06205e.eot"
+  },
+  {
+    "revision": "7a3da8567567e4fc8e4069e00306db95",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Heavy.7a3da856.woff"
+  },
+  {
+    "revision": "5c2d30d041343ac61a1fdb984a2bca8e",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-HeavyItalic.5c2d30d0.woff"
+  },
+  {
+    "revision": "713ca455a67fbe07e5ca045da1c171d9",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-HeavyItalic.713ca455.eot"
+  },
+  {
+    "revision": "86d0514b8bffd5af55b7e9afa534c42e",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-HeavyItalic.86d0514b.woff2"
+  },
+  {
+    "revision": "2e6a20a2e78a5d5a7aa25fe5c8c8847d",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Italic.2e6a20a2.woff"
+  },
+  {
+    "revision": "9f8aba7c0e9161ac914a6a45091a74e2",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Italic.9f8aba7c.eot"
+  },
+  {
+    "revision": "b191048347d96036ac2898b4844a4f3c",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Italic.b1910483.woff2"
+  },
+  {
+    "revision": "24f8471b166f86cf161ef46115903906",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Light.24f8471b.woff"
+  },
+  {
+    "revision": "51dfdeb44e3f1b98fedae28b80a83889",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Light.51dfdeb4.eot"
+  },
+  {
+    "revision": "640d984c6450fdd713f87950e965a524",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Light.640d984c.woff2"
+  },
+  {
+    "revision": "1128974f87c2fee5bb470c005faad498",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-LightItalic.1128974f.eot"
+  },
+  {
+    "revision": "a184cb3675346e21faf63704e94cea0b",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-LightItalic.a184cb36.woff2"
+  },
+  {
+    "revision": "cca3ac35a38b228e26e22b9312df2df1",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-LightItalic.cca3ac35.woff"
+  },
+  {
+    "revision": "80a22a07ca1ed245f9537c212c401f1a",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Medium.80a22a07.woff2"
+  },
+  {
+    "revision": "b17ad1fc0cf7a7d3af8f0bc1833d08e7",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Medium.b17ad1fc.woff"
+  },
+  {
+    "revision": "bb87d3f3c61325cfda29534c0072d68d",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Medium.bb87d3f3.eot"
+  },
+  {
+    "revision": "12b87f058fa25adac0c039bdd9f23c2a",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Roman.12b87f05.eot"
+  },
+  {
+    "revision": "1f9cc9acceedb19f719a2ad3e3d0fa99",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Roman.1f9cc9ac.woff2"
+  },
+  {
+    "revision": "366605da5d506128429a73a8a1feb684",
+    "url": "/LeuloProjectv2.0/static/media/HelveticaNeueCyr-Roman.366605da.woff"
+  },
+  {
+    "revision": "1fd69b32af5367f9e64d9cf36ffeaf43",
+    "url": "/LeuloProjectv2.0/static/media/Imagotipo.1fd69b32.png"
+  },
+  {
+    "revision": "f3ee55c47ff7723946e2572fc6c00504",
+    "url": "/LeuloProjectv2.0/static/media/WORKS.f3ee55c4.png"
+  },
+  {
+    "revision": "e158184599315548234daa5cbf6f5942",
+    "url": "/LeuloProjectv2.0/static/media/ball.e1581845.png"
+  },
+  {
+    "revision": "797fff2954753b6ec06c298e022722cc",
+    "url": "/LeuloProjectv2.0/static/media/brocoli.797fff29.png"
+  },
+  {
+    "revision": "c2e030ebe1c9d65a40a88375b25a410f",
+    "url": "/LeuloProjectv2.0/static/media/dewelixir.c2e030eb.png"
+  },
+  {
+    "revision": "7ec0a17b6a699a87b87dc1264131add9",
+    "url": "/LeuloProjectv2.0/static/media/down-arrow.7ec0a17b.svg"
+  },
+  {
+    "revision": "ea585a9539432609bbcbb6282ef93bf1",
+    "url": "/LeuloProjectv2.0/static/media/footer.ea585a95.png"
+  },
+  {
+    "revision": "3b0d33d9a9343c283441c1360cd82bf1",
+    "url": "/LeuloProjectv2.0/static/media/kiwi.3b0d33d9.png"
+  },
+  {
+    "revision": "f77fe8f7432c8110f366aaf2a3983a51",
+    "url": "/LeuloProjectv2.0/static/media/leulo.f77fe8f7.png"
+  },
+  {
+    "revision": "a43c79a88a9cf7ee8acb41940453add1",
+    "url": "/LeuloProjectv2.0/static/media/location.a43c79a8.png"
+  },
+  {
+    "revision": "9ff8dcccef91fce66e943912d559dc65",
+    "url": "/LeuloProjectv2.0/static/media/model.9ff8dccc.png"
+  },
+  {
+    "revision": "f7e98f97d38091e517abb1d09e87d5dd",
+    "url": "/LeuloProjectv2.0/static/media/slide1.f7e98f97.png"
+  },
+  {
+    "revision": "d24d225f86d50e188a667365119818dc",
+    "url": "/LeuloProjectv2.0/static/media/slide2.d24d225f.png"
+  },
+  {
+    "revision": "3d42828051494e5792baed8f373e6cf9",
+    "url": "/LeuloProjectv2.0/static/media/vitaminc.3d428280.png"
+  }
+]);
